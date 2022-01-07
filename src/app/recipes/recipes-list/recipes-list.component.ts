0cb75f3f2aac439a1257b48a040d65d205caabb0
Mojca Colnik <mojca.c13@gmail.com>
2022-01-07 18:29:30 +0100

    feat(): add recipe and shopping components and child components, create recipe model

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,15 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css'],
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('Test Recipe', 'this is a test', '#')];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
